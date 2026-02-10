40aec536e346526d2801c5c4106e094b553f6f61
23127jk <23127jk@hvhs.school.nz>
2026-02-10 14:53:53 +1300

    cc

--- t01_create_sprite.js
+++ t01_create_sprite.js
@@ -8,22 +8,14 @@
 // setup()
 /*******************************************************/
 function setup() {
+		new Canvas(250, 100);
+
+
 	console.log("setup: ");
 cube = new Sprite(100, 100, 100, 100);
 
 cube.color = 'blue';
-background('blue');
-circle= new sprite(50, 50, 50);
 
-circle.color = 'red';
-background("red")
-
-	new Canvas(250, 100);
-
-function update() {
-
-	background('grey');
-}
 }
 
 /*******************************************************/
@@ -31,7 +23,6 @@ function update() {
 /*******************************************************/
 function draw() {
 background.color("red")
-background.color("blue")
 }
 
 /*******************************************************/
