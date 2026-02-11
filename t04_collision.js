aad79634f9834b0d70ea1739cf79c1127d2b98a4
23127jk <23127jk@hvhs.school.nz>
2026-02-12 10:59:51 +1300

    boom

--- t04_collision.js
+++ t04_collision.js
@@ -9,14 +9,42 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+	cnv = new Canvas(755, 755);
+	world.gravity.y = 10;
+	world.gravity.x = 5;
+	console.log("setup: ");
+//cube = new Sprite(100, 100, 100, 100);
 
+//cube.color = '#87CEEB';
+//rectangle = new Sprite(200, 100, 200, 200, '200');
+
+//rectangle.color = '#56be5b';
+
+//rectangle.rotationSpeed = -2;
+//rectangle.vel.x = 1;
+
+//square = new Sprite(300, 100, 100, 100, '100');
+//square.color = '#eef3f5';
+
+//square.rotationSpeed = 2;
+cube2 = new Sprite(300, 100, 100, 100, );
+
+cube2.color = '#b065ee';
+
+cube2.rotationSpeed = -2;
+
+cube2.vel.x = 2;
+
+platform_1 = new Sprite(300, 300, 200, 10, '1000');
+platform_1.rotationSpeed = 0.5;
+platform_1.rotationSpeed = -1;
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+background(255, 100, 100)
 }
 
 /*******************************************************/
